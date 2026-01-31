77c87d51e2735fbaba8346e6de9b6feb1ed1defe
Manika Sharma <manika.sharma10000@gmail.com>
2026-01-31 09:13:44 +0100

    Added GalleryManager class to handle image selection

--- app/src/main/java/com/example/imageeditingapp/MainActivity.kt
+++ app/src/main/java/com/example/imageeditingapp/MainActivity.kt
@@ -1,16 +1,48 @@
 package com.example.imageeditingapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.imageeditingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var galleryManager: GalleryManager
+
+    /*
+        OnCreate:
+        1. Initialize binding
+        2. Display Image on Screen with crop and rotate features
+     */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        manageGallery()
     }
+
+    /*
+    manageGallery:
+        1. Store the instance of class Gallery Manager for this session
+        2. On click of select image button, request permission to open the gallery
+        3. Display/Deny depending on the action taken by Gallery Manager
+     */
+    private fun manageGallery() {
+
+        fun displayImage(uri: Uri) {
+            binding.imageToEdit.setImageURI(uri)
+            binding.appFeatures.visibility = View.VISIBLE
+        }
+
+        galleryManager = GalleryManager(this, { uri -> displayImage(uri) } )
+
+        binding.buttonSelectImage.setOnClickListener {
+            galleryManager.requestPermission()
+        }
+    }
+
 }
