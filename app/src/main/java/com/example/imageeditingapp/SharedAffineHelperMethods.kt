9bd97a1b6cc2405d727a39576f2732a2fe93e76c
Manika Sharma <manika.sharma10000@gmail.com>
2026-02-03 20:45:20 +0100

    refactor base view - base view only for system lifecycle methods

---
+++ app/src/main/java/com/example/imageeditingapp/SharedAffineHelperMethods.kt
@@ -0,0 +1,56 @@
+package com.example.imageeditingapp
+
+import android.graphics.RectF
+import android.util.Log
+
+class SharedAffineHelperMethods(private val baseView: AffineImageTransformView) {
+
+    // To update image rectangle before reading an image rectangle
+    fun updateImageRectangle() {
+        val drawable = baseView.drawable ?: return
+        baseView.imageRectangle.set(
+            0f, 0f,
+            drawable.intrinsicWidth.toFloat(),
+            drawable.intrinsicHeight.toFloat()
+        )
+        baseView.imageMatrix.mapRect(baseView.imageRectangle)
+    }
+
+    // Check if image covers crop rectangle
+    fun isImageCoverCropRectangle(
+        imageRectangle: RectF,
+        cropRectangle: RectF
+    ): Boolean {
+        return imageRectangle.left <= cropRectangle.left &&
+                imageRectangle.top <= cropRectangle.top &&
+                imageRectangle.right >= cropRectangle.right &&
+                imageRectangle.bottom >= cropRectangle.bottom
+    }
+
+    fun logBounds() {
+        Log.d("IMAGEVIEWBOUNDS", "L:$baseView.left T:$baseView.top R:$baseView.right B:$baseView.bottom")
+        Log.d("IMAGEBOUNDS", "W:${baseView.imageRectangle.width()}, H:${baseView.imageRectangle.height()},L:${baseView.imageRectangle.left} T:${baseView.imageRectangle.top} R:${baseView.imageRectangle.right} B:${baseView.imageRectangle.bottom}")
+    }
+
+    // Translate the image if error between image and crop rectangle
+    fun constraintCropRectToImage() {
+        updateImageRectangle()
+
+        val dx = when {
+            baseView.imageRectangle.left > baseView.cropRectangle.left -> baseView.cropRectangle.left - baseView.cropRectangle.left
+            baseView.imageRectangle.right < baseView.cropRectangle.right -> baseView.cropRectangle.right - baseView.cropRectangle.right
+            else -> 0f
+        }
+
+        val dy = when {
+            baseView.imageRectangle.top > baseView.cropRectangle.top -> baseView.cropRectangle.top - baseView.cropRectangle.top
+            baseView.imageRectangle.bottom < baseView.cropRectangle.bottom -> baseView.cropRectangle.bottom - baseView.cropRectangle.bottom
+            else -> 0f
+        }
+
+        if (dx == 0f && dy == 0f)
+            return
+
+        baseView.imageMatrix.postTranslate(dx, dy)
+    }
+}
