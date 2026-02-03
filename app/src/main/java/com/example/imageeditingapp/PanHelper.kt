5e71021f65e2968431d1e0090578baafa396468a
Manika Sharma <manika.sharma10000@gmail.com>
2026-02-03 21:45:13 +0100

    Pan without crop support added

---
+++ app/src/main/java/com/example/imageeditingapp/PanHelper.kt
@@ -0,0 +1,32 @@
+package com.example.imageeditingapp
+
+import android.view.MotionEvent
+
+class PanHelper(private val baseView: AffineImageTransformView) {
+    private var prevTouchX = 0f
+    private var prevTouchY = 0f
+
+    // Translate by the drag
+    fun handlePan(event: MotionEvent): Boolean {
+        when (event.actionMasked) {
+            MotionEvent.ACTION_DOWN -> { // On-Finger-Touch
+                prevTouchX = event.x
+                prevTouchY = event.y
+            }
+            MotionEvent.ACTION_MOVE -> { // On-Finger-Move
+                val curTouchX = event.x
+                val curTocuhY = event.y
+                val dx = curTouchX - prevTouchX
+                val dy = curTocuhY - prevTouchY
+                baseView.imageMatrix.postTranslate(dx, dy)
+                prevTouchX = event.x
+                prevTouchY = event.y
+                baseView.invalidate()
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> { // On-Finger-Interaction-End
+            }
+        }
+
+        return true
+    }
+}
